9dce860e96fc3c9a7fa636ab5c2735697380d471
hxreborn <hxreborn@duck.com>
2026-09-02 15:18:49 +0200

    chore(release): bump version to 2.0.0
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.drdisagree.pixellauncherenhanced"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "2.0.0"
         base.archivesName = "PLELite v${defaultConfig.versionName}"
     }
 
